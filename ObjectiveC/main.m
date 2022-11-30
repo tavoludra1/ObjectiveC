f677b0fca5d70549aa8213ebf4a230395b099245
gustavopatino <117297646+gustavopatino@users.noreply.github.com>
2022-11-30 11:43:51 -0500

    Initial Commit

---
+++ ObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveC
+//
+//  Created by GAPT on 30/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
