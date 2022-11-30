176658109c1f7882db9bdfbccc8dcf4ee7456a20
gustavopatino <117297646+gustavopatino@users.noreply.github.com>
2022-11-30 14:41:46 -0500

    Primera clase en el formato de Objective C con interfaces e implementation

--- ObjectiveC/main.m
+++ ObjectiveC/main.m
@@ -6,18 +6,83 @@
 //
 
 /*
- Simple Programa que muestra una fraccion
+ Primera clase en objetive C para fracciones
+ - Se declaran los metodos en la seccion de interface y
+ - se implementan en la seccion implementacion
  */
 
 #import <Foundation/Foundation.h>
 
-int main (int argc, const char *argv[])
+//---- @interface section ----
+
+// Describir la clase y sus metodos
+
+@interface Fraction : NSObject
+
+
+// declaracion de metodos
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+
+
+//---- @implementation section ----
+
+// Describir las variables de instancia que seran almacenadas en la variable
+// Implementacion del codigo de los metodos que son declarados en la interface
+
+
+
+@implementation Fraction
+{
+    // declaracion de los miembros de clase - variables de instancia
+    int numerator;
+    int denominator;
+}
+
+
+//---- Definicion de los Metodos ----
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator:(int)n
+{
+    numerator = n;
+}
+
+-(void) setDenominator:(int)d
+{
+    denominator = d;
+}
+
+@end
+
+
+//---- Progam section ----
+
+int main(int argc, const char *argv[])
 {
     @autoreleasepool {
-        int numerator = 3;
-        int denominator = 1;
-        double result = numerator / denominator ;
-        NSLog(@"The fraction is %i/%i = %f", numerator, denominator, result);
+        Fraction *myFraction; // objeto
+        
+        // Creando una instancia de una fraccion
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        // agregar datos a la variable myFraction 1/3
+        [myFraction setNumerator: 3];
+        [myFraction setDenominator: 1];
+        
+        // Mostrar el resultado de la fraccion utilizando el metodo print
+        NSLog(@"El valor de myFraction es: ");
+        [myFraction print];
+        
     }
     
     return 0;
